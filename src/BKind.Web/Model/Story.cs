0dad204609cbf2378b7e873c383cd1e025de0c23
Hrvoje <hrvoje.hudoletnjak@gmail.com>
2016-11-20 22:53:49 +0000

    Added story model

---
+++ src/BKind.Web/Model/Story.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace BKind.Web.Model
+{
+    public class Story : Entity
+    {
+        public string Title { get; set; }
+        public string Content { get; set; }
+     
+        public DateTime Created { get; set; }
+        public DateTime Modified { get; set; }
+        public Status Status { get; set; }
+        public int ThumbsUp { get; set; }
+        public DateTime Deleted { get; set; }
+    }
+
+
+    public enum Status
+    {
+        Draft, Published, Declined
+    }
+
+    public abstract class Entity
+    {
+        public int ID { get; set; }
+    }
+}
