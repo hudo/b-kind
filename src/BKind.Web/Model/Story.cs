d32716451704526cc970add468b49560e9ce52eb
Hudoletnjak <Hrvoje.Hudoletnjak@hmhco.com>
2017-01-25 15:55:00 +0000

    Removed duplicate classes

--- src/BKind.Web/Model/Story.cs
+++ src/BKind.Web/Model/Story.cs
@@ -21,35 +21,5 @@ namespace BKind.Web.Model
     {
         Draft, Published, Declined
     }
-
-    public class User : Entity
-    {
-        public string Username { get; set; }
-        public string FirstName { get; set; }
-        public string LastName { get; set; }
-        public virtual IEnumerable<Role> Roles { get; set; }
-        public virtual Credential Credential { get; set; }
-        public DateTime Registered { get; set; }
-        public DateTime LastLogin { get; set; }
-
-    }
-
-    public abstract class Role : Entity { }
-
-    public class Visitor : Role { }
-    public class Administrator : Role { }
-    public class Author : Role { }
-    public class Reviwer : Role { }
-
-    public class Credential : Entity
-    {
-        public string Username { get; set; }
-        public string PasswordHash { get; set; }
-        public bool IsActive { get; set; }
-    }
-
-    public abstract class Entity
-    {
-        public int ID { get; set; }
-    }
+  
 }
