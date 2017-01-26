cf6e0ed65620fd012cba541755970d0830010f1d
Hudoletnjak <Hrvoje.Hudoletnjak@hmhco.com>
2017-01-26 11:50:29 +0000

    Role names added

--- src/BKind.Web/Model/Role.cs
+++ src/BKind.Web/Model/Role.cs
@@ -1,9 +1,28 @@
 ﻿namespace BKind.Web.Model
 {
-    public abstract class Role : Entity { }
+    public abstract class Role : Entity
+    {
+        public abstract string Name { get; }
+    }
 
-    public class Visitor : Role { }
-    public class Administrator : Role { }
-    public class Author : Role { }
-    public class Reviwer : Role { }
+    public class Visitor : Role
+    {
+        public override string Name => "Visitor";
+    }
+
+    public class Administrator : Role
+    {
+        public override string Name => "Admin";
+    }
+
+    public class Author : Role
+    {
+        public override string Name => "Author";
+    }
+
+    public class Reviewer : Role
+    {
+        public override string Name => "Reviewer";
+    }
+
 }
