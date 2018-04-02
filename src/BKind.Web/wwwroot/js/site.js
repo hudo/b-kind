e578dad49f2721f8adb84b115434badd56f9270e
Ivana <idukic.hr@gmail.com>
2018-04-02 11:25:04 +0100

    Set initial responsive layout and style for the context menu.

--- src/BKind.Web/wwwroot/js/site.js
+++ src/BKind.Web/wwwroot/js/site.js
@@ -5,13 +5,21 @@ $(function () {
         $(".ra-profile-menu").toggleClass("open");
     });
 
+    $("#ra-menu-toggle").on("click", function () {
+        $(".ra-context-menu").toggleClass("open");
+    });
+
 });
 
 $(document).mouseup(function (e) {
-    var subject = $("#ra-profile-btn");
+    var itemLg = $("#ra-profile-btn");
+    var itemSm = $("#ra-menu-toggle");
 
-    if (e.target.id != subject.attr('id') && !subject.has(e.target).length) {
-        // subject.fadeOut();
+    if (e.target.id != itemLg.attr('id') && !itemLg.has(e.target).length) {
         $(".ra-profile-menu").removeClass("open");
+    }
+
+    if (e.target.id != itemSm.attr('id') && !itemSm.has(e.target).length) {
+        $(".ra-context-menu").removeClass("open");
     }
 });
